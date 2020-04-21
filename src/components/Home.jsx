34444bf22f7948579f744ab755b5f452fc07520d
sourav santra <souravsantra59580@gmail.com>
2020-04-21 20:35:05 +0530

    second commit(21/04/2020)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,19 +4,22 @@ function Home() {
     <React.Fragment>
       <div
         className="container-fluid p-4"
-        style={{ backgroundColor: "#0A2B4E", width: "100%", height: "100vh" }}
+        style={{ backgroundColor: "#071a52 ", width: "100%", height: "100vh" }}
       >
         <div class="row mt-5">
           <div class="col-md-6 m-auto">
-            <div class="card card-body text-center">
+            <div class="card card-body text-center text-light" style={{backgroundColor:'#070d59'}}>
               <h1>
                 <i class="fab fa-node-js fa-3x"></i>
               </h1>
               <p>Create an account or login</p>
+              <a href="/register" class="btn btn-success btn-block mb-2">
+                Play Now
+              </a>
               <a href="/register" class="btn btn-danger btn-block mb-2">
                 Register
               </a>
-              <a href="/login" class="btn btn-info btn-block">
+              <a href="/login" class="btn btn-primary btn-block">
                 Login
               </a>
             </div>
